ceb2a25eae73fd267c3c0011d1a466a151b979c5
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-09-03 23:23:34 -0300

    add v1__schema

--- src/main/resources/db/migration/v1__create_gym_schema.sql
+++ src/main/resources/db/migration/v1__create_gym_schema.sql
@@ -19,20 +19,70 @@ CREATE TABLE students
     updated_at    TIMESTAMP
 );
 
-CREATE TABLE modalities(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    active BOOLEAN NOT NULL DEFAULT TRUE
+CREATE TABLE modalities
+(
+    id     BIGSERIAL PRIMARY KEY,
+    name   VARCHAR(100) NOT NULL UNIQUE,
+    active BOOLEAN      NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE graduation(
-    id BIGSERIAL PRIMARY KEY,
-    modality_id BIGINT NOT NULL REFERENCES modalities(id),
-    name VARCHAR(100) NOT NULL,
-    UNIQUE (modality_id,name)
+CREATE TABLE graduations
+(
+    id          BIGSERIAL PRIMARY KEY,
+    modality_id BIGINT       NOT NULL REFERENCES modalities (id),
+    name        VARCHAR(100) NOT NULL,
+    UNIQUE (modality_id, name)
 );
 
-CREATE TABLE plans(
-    id BIGSERIAL PRIMARY KEY,
+CREATE TABLE plans
+(
+    id             BIGSERIAL PRIMARY KEY,
+    modality_id    BIGINT         NOT NULL REFERENCES modalities (id),
+    name           VARCHAR(100)   NOT NULL,
+    monthly_amount NUMERIC(10, 2) NOT NULL CHECK (monthly_amount >= 0),
+    active         BOOLEAN        NOT NULL DEFAULT TRUE,
+    UNIQUE (modality_id, name)
+);
 
+CREATE TABLE enrollments
+(
+    id              BIGSERIAL PRIMARY KEY,
+    student_id      BIGINT      NOT NULL REFERENCES students (id),
+    enrollment_date DATE        NOT NULL DEFAULT CURRENT_DATE,
+    day_maturity    DATE,
+    status          VARCHAR(20) NOT NULL DEFAULT 'active',
+    CHECK (status IN ('ACTIVE', 'CLOSED', 'CANCELED'))
+);
+
+CREATE TABLE enrollments_modalities
+(
+    id            BIGSERIAL PRIMARY KEY,
+    enrollment_id BIGINT NOT NULL REFERENCES enrollments (id),
+    modality_id   BIGINT NOT NULL REFERENCES modalities (id),
+    graduation_id BIGINT NOT NULL REFERENCES graduations (id),
+    plan_id       BIGINT NOT NULL REFERENCES plans (id),
+    start_date    DATE   NOT NULL DEFAULT CURRENT_DATE,
+    end_date      DATE,
+    UNIQUE (enrollments_id, graduation_id)
+);
+
+CREATE TABLE enrollment_invoices
+(
+    id                BIGSERIAL PRIMARY KEY,
+    enrollment_id     BIGINT         NOT NULL REFERENCES enrollments (id),
+    due_date          DATE           NOT NULL,
+    value             NUMERIC(10, 2) NOT NULL CHECK ( value >= 0 ),
+    payment_date      TIMESTAMP,
+    cancellation_date DATE,
+    status            VARCHAR(20)    NOT NULL DEFAULT 'OPEN',
+    CHECK ( status IN ('OPEN', 'PAID', 'CANCELED', 'EXPIRED') ),
+    UNIQUE (enrollment_id, due_date)
+);
+
+CREATE TABLE regularity
+(
+    id            BIGSERIAL PRIMARY KEY,
+    enrollment_id BIGINT    NOT NULL REFERENCES enrollments (id),
+    entry_date    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    exit_date     TIMESTAMP
 )
